daa694a3a165ae461c710cced1d3a28cbf786707
tharindumunaweera <tharindumunaweera94@gmail.com>
2024-05-23 20:43:25 +0530

    text with flux

--- src/test/java/com/tharindumunaweera/springbootreactive/MonoFluxTest.java
+++ src/test/java/com/tharindumunaweera/springbootreactive/MonoFluxTest.java
@@ -1,6 +1,7 @@
 package com.tharindumunaweera.springbootreactive;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MonoFluxTest {
@@ -10,6 +11,14 @@ public class MonoFluxTest {
         Mono<?> monoString = Mono.just("Tharindumunaweera")
                 .then(Mono.error(new RuntimeException("Exception occured")))
                 .log();
-        monoString.subscribe(System.out::println);// subscriber should subscribe from publisher as a first step
+        monoString.subscribe(System.out::println, (e) -> System.out.println(e.getMessage()));// subscriber should subscribe from publisher as a first step
+    }
+
+    @Test
+    public void testFlux() {
+        Flux<String> fluxString = Flux.just("spring", "spring boot", "microservice").log();
+        fluxString.subscribe((e) -> {
+            System.out.println(e);
+        });
     }
 }
